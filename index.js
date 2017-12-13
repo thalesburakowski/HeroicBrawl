f85aaa404585fbb6254f1d8eeafc9d9691dd3416
Thales Vinicius <thalesburakowski@gmail.com>
2017-12-13 21:47:53 -0200

    banco de dados funcionando e cadastrando google oauth

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 
-// const bodyParser = require('body-parser')
-// app.use(bodyParser.urlencoded())
-
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/views'))
 
 const routes = require('./routes') // ele pega o index.js por padrão
 app.use(routes)
 
+// mongoDB
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+mongoose.connect(keys.mongoURI)
+require('./models/user')
+
+
 require('./services/passport')
 require('./routes/authRoutes')(app)
+
+
+// const bodyParser = require('body-parser')
+// app.use(bodyParser.urlencoded())
+
 
 app.listen(port, () => console.log("Running..."))

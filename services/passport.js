ac707df2b1bc902e1ff8960e0318c0462385f23c
Thales Vinicius <thalesburakowski@gmail.com>
2017-12-16 21:23:45 -0200

    passport async await

--- services/passport.js
+++ services/passport.js
@@ -6,39 +6,40 @@ const keys = require('../config/keys')
 // access the model of users that it already have been created
 const User = mongoose.model('users')
 
-passport.serializeUser((user,done) =>{
+passport.serializeUser((user, done) => {
     done(null, user.id)
 })
 
 // parameter: id -> the user.id from serializeUser
-passport.deserializeUser((id, done) =>{
+passport.deserializeUser((id, done) => {
     User.findById(id).then(user => {
-            done(null, user)
-        })
+        done(null, user)
+    })
 })
 
 passport.use(
     new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (acessToken, refreshToken, profile, done) =>{
-    
-    console.log('Profile: ', profile)
-    User.findOne({ googleId: profile.id})
-        .then(existingUser =>{
-            if(existingUser){
-                // profile already exists
-                done(null, existingUser )
-            }else{
-                new User({googleId: profile.id,
-                    name: profile.name.givenName,
-                    lastName: profile.name.familyName}).save()
-                    .then(user => done(null, user))
-            }
-        })
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, async(acessToken, refreshToken, profile, done) => {
 
-    
-})) // add a new type of strategy to passport 
+        console.log('Profile: ', profile)
+        const existingUser = await User.findOne({
+            googleId: profile.id
+        })
+        if (existingUser) // profile already exists
+            return done(null, existingUser)
+        
+
+        // if profile doesn't exists
+        const user = await new User({
+            googleId: profile.id,
+            name: profile.name.givenName,
+            lastName: profile.name.familyName
+        }).save()
+        done(null, user)
+
+    })) // add a new type of strategy to passport 
 
 // don't need to export beucase it is waiting to be executed
